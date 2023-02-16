9b3f2cfc035648a827124510b9b0d2dc6320a50b
chumardo <dachigiorgadze98@gmail.com>
2023-02-16 21:08:16 +0400

    add page titles

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -5,14 +5,29 @@ import Login from "../components/pages/Login.vue";
 import Register from "../components/pages/Register.vue";
 
 const routes = [
-    { path: "/", name: "Home", component: Home },
-    { path: "/login", name: "Login", component: Login },
-    { path: "/register", name: "Register", component: Register },
+    { path: "/", name: "Home", component: Home, meta: { title: "Home" } },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+        meta: { title: "Login" },
+    },
+    {
+        path: "/register",
+        name: "Register",
+        component: Register,
+        meta: { title: "Register" },
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = to.name;
+    next();
 });
 
 export default router;
