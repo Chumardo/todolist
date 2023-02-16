44d0337b581e21563fded18249d3a243da27928d
chumardo <dachigiorgadze98@gmail.com>
2023-02-16 20:13:38 +0400

    add logout handler

--- vue-frontend/src/components/Header.vue
+++ vue-frontend/src/components/Header.vue
@@ -25,11 +25,13 @@
                     >
                 </div>
                 <div v-else>
-                    <router-link
+                    <button
+                        @click="authStore.handleLogout"
                         to="/logout"
                         class="text-blue-400 hover:bg-blue-400 hover:text-black focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
-                        >Log Out</router-link
                     >
+                        Log Out
+                    </button>
                 </div>
             </div>
         </nav>
